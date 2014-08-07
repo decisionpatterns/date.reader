d03ed6b74995b51c15f49eecc72ac74b130a82c0
Daryl McCullough <dmccullough@decisionpatterns.com>
2014-08-07 09:08:11 -0700

    SR-4 Fixed as.POSIXct.character (and related functions) so that it works with the all-digits formats: YYYYMMDD YYYYMMDD_HH YYYYMMDD_HHMM YYYYMMDD_HHMMSS
    Also, added a new function can.posix, which takes a vector of character
    and returns TRUE if all the values can be parsed using the same format.

--- R/can.posix.R
+++ R/can.posix.R
@@ -2,8 +2,17 @@
 #' 
 #' @param x vector of character; values to parse
 #'
-#' @return vector of logical. If the values
-#' can be interpreted as dates, it returns TRUE
+#' @param nTrials integer; number of strings to check before deciding
+#' the format
+#' 
+#' @param nErrors integer; the number of unparsable strings to allow. 
+#' If the number of unparseable strings exceeds nErrors, return NA,
+#' which means that the strings are either not dates, or not a consistent
+#' format.
+#'
+#' @return logical. If the values can be interpreted as dates (with
+#' the same format), it returns TRUE. It returns false if they cannot 
+#' be interpreted as dates, or if there is no consistent format.
 #'
 #'   
 #' @export
