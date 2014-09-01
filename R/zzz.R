03bad169b68e12e6a8ac72f84f65d31d3e7ea76e
Christopher Brown <cbrown@decisionpatterns.com>
2014-09-01 10:18:11 -0700

    Add R/zzz.R for start-up message

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onAttach <- function( libname, pkgname ) {
+
+  packageStartupMessage( 
+    pkgname ,
+    "-" ,
+    utils::packageVersion(pkgname, libname),
+    " by Decision Patterns" ,
+    domain = NA
+  )
+
+}
