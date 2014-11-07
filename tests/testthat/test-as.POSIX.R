f4f0c2e818b9e9c8be9fb15094b033187cfccad6
Christopher Brown <cbrown@decisionpatterns.com>
2014-11-07 07:08:00 -0800

    Version 0.3
    -Fix tests
    - Commit osr-29, osr-43, osr-47, osr-50

--- tests/testthat/test-as.POSIX.R
+++ tests/testthat/test-as.POSIX.R
@@ -1,5 +1,9 @@
 library(testthat)
 library(lubridate)
+
+
+context( "test-as.POSIX.R")
+
 dts.1 <- c( '20140210', '19791118', '19720329' ) 
 
 dts.2 <- c('January 31, 2011', 'March 23, 1957', 'October 26, 1929')
@@ -11,10 +15,9 @@ dts.4 <- c("10/12/25", "14/11/13", "09/03/29")
 z1 <- as.POSIXct.character(dts.1)
 z2 <- as.POSIXct.character(dts.2)
 z3 <- as.POSIXct.character(dts.3)
-z4 <- as.POSIXct.character(dts.4)
+z4 <- as.POSIXct.character(dts.4, )
 
 w1 <- ymd(dts.1)
 w2 <- mdy(dts.2)
 w3 <- mdy_hm(dts.3)
 w4 <- ymd(dts.4)
-print("done with test-as.POSIX.R")
