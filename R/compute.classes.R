9a403a6eb8c981c89a814e0d8b973a60f544f4e4
Christopher Brown <cbrown@decisionpatterns.com>
2015-02-25 05:21:41 -0800

    Version 0.4.3
    - DATER-33: fix bug in parse.dates
    - Fix formatting for parse.dates
    - Word-smith docs for parse.dates

--- R/compute.classes.R
+++ R/compute.classes.R
@@ -12,14 +12,16 @@ NULL
 #' @param x data; the data table or data frame
 #'
 #' @note Internal function, a wrapper for lubridate functions
+#' 
 #' @note If a vector input is given, the values that cannot be parsed
 #' will be given the value NA
 #'
 #' @return 
 #' POSIXct object representing a date (or datetime), or NA
 #' if the input cannot be parsed.
+#' 
 #' @examples 
-#'   # Named arguments
+#' # Named arguments
 #'   x <- list(age="numeric", name="character", state="character")
 #'   df <- data.frame(
 #'    name=c("fred", "barney"), 
@@ -27,14 +29,14 @@ NULL
 #'    ssn=c("123-45-6789", "012-34-5678"),
 #'    weight=c(214, 145))
 #'   x <- date.reader:::.compute.classes(x,df)
-#'   # Note should be c("character", "numeric", NA, NA)
+#' # Note should be c("character", "numeric", NA, NA)
 #'   
-#'   # Unnamed arguments
+#' # Unnamed arguments
 #'   x <- list("character", "numeric")
 #'   x <- date.reader:::.compute.classes(x,df) 
 #'   # Note should be c("character", "numeric", "character", "numeric")
 #'   
-#'   # NULL argument
+#' # NULL argument
 #'   x <- NULL
 #'   x <- date.reader:::.compute.classes(x,df) 
 #'   # Note should be c(NA, NA, NA, NA)
--- R/parse.dates.R
+++ R/parse.dates.R
@@ -1,127 +1,150 @@
 #' parse.dates
+#'
+#' parse columns into 
 #' 
+#' @param x object; the object to parse dates  
 #' @param ... additional arguments passed to \code{utils::read.table}
-#' @param x data; the table to fix
 #'
 #' @return data frame or data table
 #' 
-#' If a character column can be interpreted as dates, then translate
-#' values into POSIXct objects.
+#' \code{parse.dates} is an S3 generic function for parsing dates from an 
+#' existing object. For parsing dates while reading see 
+#' \code{\link[date.reader]{read.table}}.  
+#' 
+#' If a character or factor column can be interpreted as dates, it is 
+#' coerces into POSIXct object
 #' 
 #' @seealso 
 #'   \code{\link[base]{as.POSIXct}}
 #'   
 #' @examples
-#'   name <- c("bob", "fred", "sally")
-#'   birthday <- c("01/22/1993", "02/25/1980", "03/31/1970")
-#'   birthday <- as.factor(birthday)
-#'   table <- data.frame(name,birthday)
-#'   table1 <- data.table(table)
-#'   table.new <- parse.dates(table)
-#'   table.new1 <- parse.dates(table1)
+#'   name     <- c("bob", "fred", "sally")
+#'   birthday.character <- c("01/22/1993", "02/25/1980", "03/31/1970") 
+#'   birthday.factor <- as.factor(birthday.character)
+#'   
+#'   df  <- data.frame( name, birthday.character, birthday.factor, stringsAsFactors=FALSE )
+#'   res <- parse.dates(df)
+#'   class( res$birthday.character )                # "POSIXct" "POSIXt" 
+#'   class( res$birthday.factor )                  
+#'   
+#'   dt <- setDT( df )
+#'   parse.dates(dt)                     
+#'   class( dt$birthday.character )                # "POSIXct" "POSIXt" 
+#'   class( dt$birthday.factor )
+#'   
 #' @export
 
-parse.dates <- function( 
-  x
-  , ... 
-) 
-  UseMethod('parse.dates')
+parse.dates <- function( x, ... ) UseMethod('parse.dates')
+
 
 #' @rdname parse.dates
 #' @method parse.dates data.frame
 #' @export
 
-parse.dates.data.frame <- function(x, ...) {
+parse.dates.data.frame <- function( x, ... ) {
+  
   args <- list(...)  
   colClasses <- args[['colClasses']]
-  colClasses <- .compute.classes(colClasses, x)
-  for (i in seq_len(ncol(x))) {
+  colClasses <- .compute.classes( colClasses, x )
+  
+  for ( i in seq_len(ncol(x)) ) {
     cls <- colClasses[[i]]
-    col <- x[, i]
-    col <- parse.dates(col, colClasses = cls)
-    if (!is.na(col))
-      x[, i] <- col
+    col <- parse.dates( x[[i]], colClasses = cls )
+    if ( all(! is.na(col)) ) x[[i]] <- col
   }
   return(x)
+  
 }
+
 
 #' @rdname parse.dates
 #' @method parse.dates default
 #' @export
 
-parse.dates.default <- function(x, ...) {
+parse.dates.default <- function( x, ... ) {
   x <- as.character(x)
-  parse.dates(x, ...)
+  parse.dates( x, ... )
 }
+
 
 #' @rdname parse.dates
 #' @method parse.dates character
 #' @export
-parse.dates.character <- function(x, ...) {
+
+parse.dates.character <- function( x, ... ) {
+  
   args <- list(...)  
   colClasses <- args[['colClasses']]
-  if (is.null(colClasses)) {
-    colClasses <- NA
-  }
+  
+  if (is.null(colClasses)) colClasses <- NA
+  
+  
   colClasses <- colClasses[[1]]
   tz <- args[["tz"]]
-  if (is.null(tz)) {
+  if (is.null(tz)) 
     tz <- options::get_option(date.reader$tz, 'UTC')
-  }
-  orders <- args[["orders"]]
-  if (is.null(orders)) {
-    orders <- options::get_option(date.reader$orders, all.orders)
-  }
-  nErrors <- args[["nErrors"]]
-  if (is.null(nErrors)) {
-    nErrors <- options::get_option(date.reader$nErrors, 0)
-  }
-  autostart <- args[["autostart"]]
-  if (is.null(autostart)) {
-    autostart <- options::get_option(date.reader$autostart, 30)
-  }
   
-  if (is.na(colClasses)) {
-    force = FALSE
-  } else if (colClasses == "POSIXct") {
-    force = TRUE
-  } else {
+  orders <- args[["orders"]]
+  if (is.null(orders)) 
+    orders <- options::get_option(date.reader$orders, all.orders)
+  
+  nErrors <- args[["nErrors"]]
+  if( is.null(nErrors) ) 
+    nErrors <- options::get_option(date.reader$nErrors, 0)
+  
+  autostart <- args[["autostart"]]
+  if( is.null(autostart) ) 
+    autostart <- options::get_option(date.reader$autostart, 30)
+  
+  
+  if( is.na(colClasses) ) 
+    force <- FALSE else 
+    if (colClasses == "POSIXct") force <- TRUE else 
     return(NA)
-  }
+  
+  
   orders <- which.orders(
     x
     , force=force
     , orders=orders
     , autostart=autostart
-    , nErrors=nErrors)
-  if(is.na(orders)) {
-    return(NA)
-  }
+    , nErrors=nErrors
+  )
   
-  lubridate::parse_date_time(x, orders, tz=tz)
+  if( is.na(orders) ) return(NA)
+  
+  lubridate::parse_date_time( x, orders, tz=tz )
+  
 }
+
 
 #' @rdname parse.dates
 #' @method parse.dates data.table
+#' @import data.table
 #' @export
-parse.dates.data.table <- function(x, ...) {
+
+parse.dates.data.table <- function( x, ... ) {
+  
   args <- list(...)  
   colClasses <- args[['colClasses']]
-  colClasses <- .compute.classes(colClasses, x)
+  colClasses <- .compute.classes( colClasses, x )
   
-  parse.dates.aux <- function(col, cls) {
-    return(parse.dates(col, colClasses = cls))
-  }
+  parse.dates.aux <- 
+    
   
-  new.cols <- mapply(parse.dates.aux, x, colClasses)
-  affected <- function(col) {
-    ! all(is.na(col))
-  }
   
-  indices <- sapply(new.cols, affected)
+  new.cols <- 
+    mapply( 
+        function(col, cls) parse.dates( col, colClasses = cls )
+      , x
+      , colClasses 
+    )
+
+  indices <- sapply( new.cols,  function(col) ! all( is.na(col) ) )
   indices <- which(indices)
 
   new.cols <- new.cols[indices]
   x[, indices := new.cols, with=FALSE]
   return(x)
+  
 }
